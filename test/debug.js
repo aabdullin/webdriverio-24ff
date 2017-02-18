05670fdc8405468e148918d231f92ecabb8f944c
Artur Abdullin <rutrika@gmail.com>
2017-02-18 13:42:19 -0800

    Added debug command

---
+++ test/debug.js
@@ -0,0 +1,17 @@
+describe("Shop CTA Button", function () {
+  it("should link to the product page", function() {
+    browser.url('/')
+
+    var title = browser.getTitle()
+    console.log('Title is: ' + title);
+
+    browser.click('.shop-callout a');
+    browser.debug();
+
+    var productTitle = browser.getTitle()
+    console.log('Title is: ' + productTitle);
+
+    var url = browser.getUrl()
+    console.log('Url is ' + url);
+  });
+});
