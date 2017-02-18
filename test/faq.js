5513836ea874321cebd81fe56ff3946285dc1372
Artur Abdullin <rutrika@gmail.com>
2017-02-18 15:45:15 -0800

    New element expected to be hidden

--- test/faq.js
+++ test/faq.js
@@ -1,10 +1,22 @@
 describe("Homepage FAQ Accordion", function(){
-  it("should show first section on page load", function(){
+
+  beforeEach(function() {
     browser.url("/");
+  });
 
-
+  it("should show first section on page load", function(){
     var firstHeight = browser.getCssProperty(".accordion .accordion-item:first-child .accordion-content", "height");
 
-    console.log(firstHeight)
+    expect(firstHeight.parsed.value).to.be.greaterThan(0);
+  });
+
+  it("should not show the other content"), function(){
+    var secondHeight = browser.getCssProperty(".accordion .accordion-item:nth-of-type(2) .accordion-content", "height");
+
+    console.log(secondHeight);
+
+    expect(secondHeight.parsed.value).to.equal(0);
   })
+
+
 })
