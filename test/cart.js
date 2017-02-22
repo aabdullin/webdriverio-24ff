1c5ddf13fc19e11c0c19ae2360e70220e4cce603
Artur Abdullin <rutrika@gmail.com>
2017-02-21 18:39:18 -0800

    Added page object to for cart.js and updated wdio file

--- test/cart.js
+++ test/cart.js
@@ -1,81 +1,73 @@
-describe ("Cart functionality", function() {
-  var btn = "#buyNowButton";
-  var qty = "#qty";
+var cart = require('./cart.page.js');
 
+describe("Cart Functionality", function () {
   beforeEach(function () {
     browser.url("/product-page.html");
   });
 
-  it ('should only let you buy after setting a quantity', function () {
-    var isBtnEnabled = browser.isEnabled(btn);
+  it('should only let you buy after setting a quantity', function () {
+    var isBtnEnabled = cart.btn.isEnabled();
     expect(isBtnEnabled, "'buy now' should be disabled to begin").to.be.false;
 
-    // Add quantity
-    browser.setValue(qty, 10);
+    // Add qty
+    cart.qty.setValue(10);
 
-    isBtnEnabled = browser.isEnabled(btn);
+    isBtnEnabled = cart.btn.isEnabled();
     expect(isBtnEnabled, "'buy now' is now enabled").to.be.true;
   });
 
-
-
-  describe("checkout process", function() {
+  describe("checkout process", function () {
     beforeEach(function () {
       // Add qty
-      browser.setValue(qty, 10);
+      cart.qty.setValue(10);
 
-      browser.click(btn);
+      // Click "buy now"
+      cart.btn.click();
     });
 
-    it ('should disable buy now button during processing', function () {
+    it('should disable buy now button during processing', function () {
       // Verify "buy now" is disabled
-      var isBtnEnabled = browser.isEnabled(btn);
+      var isBtnEnabled = cart.btn.isEnabled();
       expect(isBtnEnabled, "'buy now' should be disabled after clicking").to.be.false;
 
-      var btnText = browser.getText(btn);
+      var btnText = cart.btn.getText();
       expect(btnText, "Verify 'buy now' text has changed").to.contain("Purchasing");
     });
 
-
-    it ('should show a thank you message with qty and type', function () {
-      var thankYou = ".callout*=Thank you human"
-
+    it('should show a thank you message with qty and type', function () {
       // waitForExist "thank you message"
-      browser.waitForExist(thankYou, 3000);
+      cart.thankYou.waitForExist();
 
       // verify is has proper qty and type
-      var thankText = browser.getText(thankYou);
+      var thankText = cart.thankYou.getText();
       expect(thankText).to.contain("10 T-800 Model 101");
     });
 
     it('should clear input after completion', function () {
       // waitForValue for qty input
-      browser.waitForValue(qty, 3000, true);
+      cart.qty.waitForValue(null, true);
     });
-
 
     it('should reset button text after purchase completes', function () {
       // wait for button to return back to 'buy now'
       browser.waitUntil(function () {
-        return browser.getText(btn) !== 'Purchasing...'
-      }, 3000)
-      // verify button now says 'buy now'
-      var btnText = browser.getText(btn);
-      expect(btnText).to.equal('Buy Now');
-    });
+        return cart.btn.getText() !== 'Purchasing...'
+      }, 3000);
 
+      // Verify button now says 'buy now'
+      var btnText = cart.btn.getText();
+      expect(btnText).to.equal('Buy Now');
+    })
 
     it('should hide thank you message after clicking close button', function () {
-      var thankYou = ".callout*=Thank you human";
-
       // waitForExist "thank you message"
-      browser.waitForExist(thankYou, 3000);
+      cart.thankYou.waitForExist();
 
       // Click close button
-      browser.click(".close-button");
+      $(".close-button").click();
 
-      // Use "reverse" flag to wait for it to dissappear
-      browser.waitForVisible(thankYou, 3000, true);
-    })
+      // use "reverse" flag to wait for it to disappear
+      cart.thankYou.waitForVisible(null, true);
+    });
   });
 });
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,12 +1,12 @@
-console.log ("here")
+var notifier = require('node-notifier');
 
-var baseUrl = "http://127.0.0.1:8303";
+var baseUrl = 'http://127.0.0.1:8303';
 
 if (process.env.SERVER === "prod") {
-  baseUrl = 'http://www.kevinlamping.com/webdriverio-course-content/';
+  baseUrl = 'http://www.kevinlamping.com/webdriverio-course-content';
 }
 
-var timeout = process.env.DEBUG ? 9999999 : 10000;
+var timeout = process.env.DEBUG ? 99999999 : 10000;
 
 exports.config = {
 
@@ -20,20 +20,11 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/**/*.js'
+        './test/**/*.js',
     ],
     // Patterns to exclude.
     exclude: [
-      './test/assert.js',
-      './test/bloombees.js',
-      './test/chai-assert.js',
-      './test/chai-should.js',
-      './test/chai-expect.js',
-      './test/debug.js',
-      './test/faq.js',
-      './test/shop-button.js',
-      './test/shop-button-sync.js',
-      './test/test.js',
+        // 'path/to/excluded/files'
     ],
     //
     // ============
@@ -51,18 +42,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
-    //
-    // If you have trouble getting all important capabilities together, check out the
-    // Sauce Labs platform configurator - a great tool to configure your capabilities:
-    // https://docs.saucelabs.com/reference/platforms-configurator
-    //
     capabilities: [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
         browserName: 'chrome'
     }],
     //
@@ -76,15 +56,11 @@ exports.config = {
     // e.g. using promises you can set the sync option to false.
     sync: true,
     //
-    // Level of logging ver bosity: silent | verbose | command | data | result | error
+    // Level of logging verbosity: silent | verbose | command | data | result | error
     logLevel: 'silent',
     //
     // Enables colors for log output.
     coloredLogs: true,
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
-    bail: 0,
     //
     // Saves a screenshot to a given path if a command fails.
     screenshotPath: './errorShots/',
@@ -94,7 +70,7 @@ exports.config = {
     baseUrl: baseUrl,
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 100000,
+    waitforTimeout: 10000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
@@ -135,7 +111,7 @@ exports.config = {
     framework: 'mocha',
     //
     // Test reporter for stdout.
-    // The only one supported by default is 'dot'
+    // The following are supported: dot (default), spec, and xunit
     // see also: http://webdriver.io/guide/testrunner/reporters.html
     // reporters: ['dot'],
     //
@@ -149,25 +125,24 @@ exports.config = {
     // =====
     // Hooks
     // =====
-    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+    // WedriverIO provides several hooks you can use to interfere with the test process in order to enhance
     // it and to build services around it. You can either apply a single function or an array of
     // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
     // resolved to continue.
     //
     // Gets executed once before all workers get launched.
-    // onPrepare: function (config, capabilities) {
-    // },
-    //
-    // Gets executed just before initialising the webdriver session and test framework. It allows you
-    // to manipulate configurations depending on the capability or spec.
-    // beforeSession: function (config, capabilities, specs) {
-    // },
+    onPrepare: function (config, capabilities) {
+        notifier.notify({
+            title: 'WebdriverIO',
+            message: 'Test run started'
+        })
+    },
     //
     // Gets executed before test execution begins. At this point you can access all global
     // variables, such as `browser`. It is the perfect place to define custom commands.
     before: function (capabilities, specs) {
-        expect = require('chai').expect;
-     },
+      expect = require('chai').expect;
+    },
     //
     // Hook that gets executed before the suite starts
     // beforeSuite: function (suite) {
@@ -196,8 +171,14 @@ exports.config = {
     // },
     //
     // Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-    // afterTest: function (test) {
-    // },
+    afterTest: function (test) {
+        if (!test.passed) {
+            notifier.notify({
+                title: 'Test failure!',
+                message: test.parent + ' ' + test.title
+            })
+        }
+    },
     //
     // Hook that gets executed after the suite has ended
     // afterSuite: function (suite) {
@@ -205,15 +186,15 @@ exports.config = {
     //
     // Gets executed after all tests are done. You still have access to all global variables from
     // the test.
-    // after: function (result, capabilities, specs) {
-    // },
-    //
-    // Gets executed right after terminating the webdriver session.
-    // afterSession: function (config, capabilities, specs) {
+    // after: function (capabilities, specs) {
     // },
     //
     // Gets executed after all workers got shut down and the process is about to exit. It is not
     // possible to defer the end of the process using a promise.
-    // onComplete: function(exitCode) {
-    // }
+    onComplete: function(exitCode) {
+        notifier.notify({
+            title: 'WebdriverIO',
+            message: 'Tests finished running.'
+        })
+    }
 }
