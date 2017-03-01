6600fa46ebd3409d4cedc5a441334ce3866226c7
Artur Abdullin <rutrika@gmail.com>
2017-03-01 00:25:27 -0800

    Updated wdio.js

---
+++ test/quri.js
@@ -0,0 +1,15 @@
+describe ("login", function () {
+    it ("render main header text correctly" {
+      browser.url("http://quri.com/")
+      });
+    });
+
+
+
+  /*  it ('login link is taking the user to the url' {
+
+    });
+
+    it ('')
+
+});
